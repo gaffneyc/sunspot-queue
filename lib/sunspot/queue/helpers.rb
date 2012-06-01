4aa307322edda536bb7c03ada06fde7dd8dbcc48
Chris Gaffney <gaffneyc@gmail.com>
2012-06-01 13:28:43 -0400

    Refactor without_proxy so it returns the result of the block.

--- lib/sunspot/queue/helpers.rb
+++ lib/sunspot/queue/helpers.rb
@@ -3,21 +3,19 @@ require "sunspot/queue/session_proxy"
 module Sunspot::Queue
   module Helpers
     def without_proxy
+      proxy = nil
+
       # Pop off the queueing proxy for the block if it's in place so we don't
       # requeue the same job multiple times.
       if Sunspot.session.instance_of?(SessionProxy)
         proxy = Sunspot.session
         Sunspot.session = proxy.session
-
-        begin
-          yield
-        ensure
-          Sunspot.commit rescue nil
-          Sunspot.session = proxy
-        end
-      else
-        yield
       end
+
+      yield
+    ensure
+      Sunspot.commit rescue nil
+      Sunspot.session = proxy if proxy
     end
   end
 end
